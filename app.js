9affb198ccd1f4c193c7a5f3afbc2fc891e551b4
sicxx-xnx <hpeyton33@gmail.com>
2025-07-29 17:44:25 -0500

    add first three number inputs
    added the first three number inputs in the same format at the zero input plus all operator dom inputs.

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 let firstNumberInput = ""
 let secondNumberInput = ""
 let operatorInput = ""
+const operatorContainer = document.querySelector(".operators")
+const plus = document.querySelector("#plus")
+const minus = document.querySelector("#minus")
+const divide = document.querySelector("#divide")
+const multiply = document.querySelector("multiply")
 const zero = document.querySelector("#zero")
+const one = document.querySelector("#one")
+const two = document.querySelector("#two")
+const three = document.querySelector("#three")
+one.addEventListener("click",() => {
+    if (operatorInput == "" && firstNumberInput !== "" ) {
+    firstNumberInput += "1"
+} else if (operatorInput == "" && firstNumberInput == "") {
+    firstNumberInput = "1"
+} else if (operatorInput !== "" && secondNumberInput !== "" ) {
+    secondNumberInput += "1"
+} else if (operatorInput !== "" && secondNumberInput == "") {
+    secondNumberInput = "1"
+} else { return "error, please clear the calculator"
+    
+} {
+    
+} {
+    
+} {
+    
+}    
+})
 zero.addEventListener("click",()=> {
 if (operatorInput == "" && firstNumberInput !== "" ) {
     firstNumberInput += "0"
@@ -19,6 +46,66 @@ if (operatorInput == "" && firstNumberInput !== "" ) {
     
 } {
     
+}    
+})
+two.addEventListener("click",()=> {
+if (operatorInput == "" && firstNumberInput !== "" ) {
+    firstNumberInput += "2"
+} else if (operatorInput == "" && firstNumberInput == "") {
+    firstNumberInput = "2"
+} else if (operatorInput !== "" && secondNumberInput !== "" ) {
+    secondNumberInput += "2"
+} else if (operatorInput !== "" && secondNumberInput == "") {
+    secondNumberInput = "2"
+} else { return "error, please clear the calculator"
+    
+} {
+    
+} {
+    
+} {
+    
+}    
+}) 
+three.addEventListener("click",()=> {
+if (operatorInput == "" && firstNumberInput !== "" ) {
+    firstNumberInput += "3"
+} else if (operatorInput == "" && firstNumberInput == "") {
+    firstNumberInput = "3"
+} else if (operatorInput !== "" && secondNumberInput !== "" ) {
+    secondNumberInput += "3"
+} else if (operatorInput !== "" && secondNumberInput == "") {
+    secondNumberInput = "3"
+} else { return "error, please clear the calculator"
+    
+} {
+    
+} {
+    
+} {
+    
+}    
+})
+
+
+operatorContainer.addEventListener("click",(e) => {
+target = e.target    
+switch (target.id) {
+    case "plus": operatorInput = "+"
+        
+     break;
+    case "minus": operatorInput = "-"
+        
+     break;
+    case "divide": operatorInput = "/"
+        
+     break;
+    case "multiply": operatorInput = "X"
+        
+    break;
+
+    default:
+        break;
 }    
 })
 
