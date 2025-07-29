64abc0b6d1818a50c367d2c9bcf973eced82b9aa
sicxx-xnx <hpeyton33@gmail.com>
2025-07-29 18:29:14 -0500

    add remaining number inputs
    added the remaining number inputs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let firstNumberInput = ""
 let secondNumberInput = ""
 let operatorInput = ""
+const output = document.querySelector(".output")
 const operatorContainer = document.querySelector(".operators")
 const plus = document.querySelector("#plus")
 const minus = document.querySelector("#minus")
@@ -10,15 +11,25 @@ const zero = document.querySelector("#zero")
 const one = document.querySelector("#one")
 const two = document.querySelector("#two")
 const three = document.querySelector("#three")
+const four = document.querySelector("#four")
+const five = document.querySelector("#five")
+const six = document.querySelector("#six")
+const seven = document.querySelector("#seven")
+const eight = document.querySelector("#eight")
+const nine = document.querySelector("#nine")
 one.addEventListener("click",() => {
     if (operatorInput == "" && firstNumberInput !== "" ) {
     firstNumberInput += "1"
+    output.textContent = firstNumberInput
 } else if (operatorInput == "" && firstNumberInput == "") {
     firstNumberInput = "1"
+    output.textContent = firstNumberInput
 } else if (operatorInput !== "" && secondNumberInput !== "" ) {
     secondNumberInput += "1"
+    output.textContent = secondNumberInput
 } else if (operatorInput !== "" && secondNumberInput == "") {
     secondNumberInput = "1"
+    output.textContent = secondNumberInput
 } else { return "error, please clear the calculator"
     
 } {
@@ -30,14 +41,18 @@ one.addEventListener("click",() => {
 }    
 })
 zero.addEventListener("click",()=> {
-if (operatorInput == "" && firstNumberInput !== "" ) {
+    if (operatorInput == "" && firstNumberInput !== "" ) {
     firstNumberInput += "0"
+    output.textContent = firstNumberInput
 } else if (operatorInput == "" && firstNumberInput == "") {
     firstNumberInput = "0"
+    output.textContent = firstNumberInput
 } else if (operatorInput !== "" && secondNumberInput !== "" ) {
     secondNumberInput += "0"
+    output.textContent = secondNumberInput
 } else if (operatorInput !== "" && secondNumberInput == "") {
     secondNumberInput = "0"
+    output.textContent = secondNumberInput
 } else { return "error, please clear the calculator"
     
 } {
@@ -49,14 +64,18 @@ if (operatorInput == "" && firstNumberInput !== "" ) {
 }    
 })
 two.addEventListener("click",()=> {
-if (operatorInput == "" && firstNumberInput !== "" ) {
+    if (operatorInput == "" && firstNumberInput !== "" ) {
     firstNumberInput += "2"
+    output.textContent = firstNumberInput
 } else if (operatorInput == "" && firstNumberInput == "") {
     firstNumberInput = "2"
+    output.textContent = firstNumberInput
 } else if (operatorInput !== "" && secondNumberInput !== "" ) {
     secondNumberInput += "2"
+    output.textContent = secondNumberInput
 } else if (operatorInput !== "" && secondNumberInput == "") {
     secondNumberInput = "2"
+    output.textContent = secondNumberInput
 } else { return "error, please clear the calculator"
     
 } {
@@ -68,14 +87,18 @@ if (operatorInput == "" && firstNumberInput !== "" ) {
 }    
 }) 
 three.addEventListener("click",()=> {
-if (operatorInput == "" && firstNumberInput !== "" ) {
+    if (operatorInput == "" && firstNumberInput !== "" ) {
     firstNumberInput += "3"
+    output.textContent = firstNumberInput
 } else if (operatorInput == "" && firstNumberInput == "") {
     firstNumberInput = "3"
+    output.textContent = firstNumberInput
 } else if (operatorInput !== "" && secondNumberInput !== "" ) {
     secondNumberInput += "3"
+    output.textContent = secondNumberInput
 } else if (operatorInput !== "" && secondNumberInput == "") {
     secondNumberInput = "3"
+    output.textContent = secondNumberInput
 } else { return "error, please clear the calculator"
     
 } {
@@ -86,8 +109,144 @@ if (operatorInput == "" && firstNumberInput !== "" ) {
     
 }    
 })
-
-
+four.addEventListener("click",() => {
+    if (operatorInput == "" && firstNumberInput !== "" ) {
+    firstNumberInput += "4"
+    output.textContent = firstNumberInput
+} else if (operatorInput == "" && firstNumberInput == "") {
+    firstNumberInput = "4"
+    output.textContent = firstNumberInput
+} else if (operatorInput !== "" && secondNumberInput !== "" ) {
+    secondNumberInput += "4"
+    output.textContent = secondNumberInput
+} else if (operatorInput !== "" && secondNumberInput == "") {
+    secondNumberInput = "4"
+    output.textContent = secondNumberInput
+} else { return "error, please clear the calculator"
+    
+} {
+    
+} {
+    
+} {
+    
+}    
+})
+five.addEventListener("click",() => {
+    if (operatorInput == "" && firstNumberInput !== "" ) {
+    firstNumberInput += "5"
+    output.textContent = firstNumberInput
+} else if (operatorInput == "" && firstNumberInput == "") {
+    firstNumberInput = "5"
+    output.textContent = firstNumberInput
+} else if (operatorInput !== "" && secondNumberInput !== "" ) {
+    secondNumberInput += "5"
+    output.textContent = secondNumberInput
+} else if (operatorInput !== "" && secondNumberInput == "") {
+    secondNumberInput = "5"
+    output.textContent = secondNumberInput
+} else { return "error, please clear the calculator"
+    
+} {
+    
+} {
+    
+} {
+    
+}    
+})
+six.addEventListener("click",() => {
+    if (operatorInput == "" && firstNumberInput !== "" ) {
+    firstNumberInput += "6"
+    output.textContent = firstNumberInput
+} else if (operatorInput == "" && firstNumberInput == "") {
+    firstNumberInput = "6"
+    output.textContent = firstNumberInput
+} else if (operatorInput !== "" && secondNumberInput !== "" ) {
+    secondNumberInput += "6"
+    output.textContent = secondNumberInput
+} else if (operatorInput !== "" && secondNumberInput == "") {
+    secondNumberInput = "6"
+    output.textContent = secondNumberInput
+} else { return "error, please clear the calculator"
+    
+} {
+    
+} {
+    
+} {
+    
+}    
+})
+seven.addEventListener("click",() => {
+    if (operatorInput == "" && firstNumberInput !== "" ) {
+    firstNumberInput += "7"
+    output.textContent = firstNumberInput
+} else if (operatorInput == "" && firstNumberInput == "") {
+    firstNumberInput = "7"
+    output.textContent = firstNumberInput
+} else if (operatorInput !== "" && secondNumberInput !== "" ) {
+    secondNumberInput += "7"
+    output.textContent = secondNumberInput
+} else if (operatorInput !== "" && secondNumberInput == "") {
+    secondNumberInput = "7"
+    output.textContent = secondNumberInput
+} else { return "error, please clear the calculator"
+    
+} {
+    
+} {
+    
+} {
+    
+}    
+})
+eight.addEventListener("click",() => {
+    if (operatorInput == "" && firstNumberInput !== "" ) {
+    firstNumberInput += "8"
+    output.textContent = firstNumberInput
+} else if (operatorInput == "" && firstNumberInput == "") {
+    firstNumberInput = "8"
+    output.textContent = firstNumberInput
+} else if (operatorInput !== "" && secondNumberInput !== "" ) {
+    secondNumberInput += "8"
+    output.textContent = secondNumberInput
+} else if (operatorInput !== "" && secondNumberInput == "") {
+    secondNumberInput = "8"
+    output.textContent = secondNumberInput
+} else { return "error, please clear the calculator"
+    
+} {
+    
+} {
+    
+} {
+    
+}    
+})
+nine.addEventListener("click",() => {
+    if (operatorInput == "" && firstNumberInput !== "" ) {
+    firstNumberInput += "9"
+    output.textContent = firstNumberInput
+} else if (operatorInput == "" && firstNumberInput == "") {
+    firstNumberInput = "9"
+    output.textContent = firstNumberInput
+} else if (operatorInput !== "" && secondNumberInput !== "" ) {
+    secondNumberInput += "9"
+    output.textContent = secondNumberInput
+} else if (operatorInput !== "" && secondNumberInput == "") {
+    secondNumberInput = "9"
+    output.textContent = secondNumberInput
+} else { return "error, please clear the calculator"
+    
+} {
+    
+} {
+    
+} {
+    
+}    
+})
 operatorContainer.addEventListener("click",(e) => {
 target = e.target    
 switch (target.id) {
@@ -129,3 +288,15 @@ divide : function(a,b) {return a/b},
 multiply : function(a,b) {return a*b},    
 }
 
+
+
+function runEquation(a,b) {
+    switch (operatorInput) {
+        case "+": mathfunction.add(a,b)
+            
+            break;
+    
+        default:
+            break;
+    }
+}
